365c18129c61ba5c98abd7bc3b67801614584a2b
sohit007 <161561338+sohit007@users.noreply.github.com>
2024-05-21 10:52:19 +0530

    info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 AI = is_enabled((environ.get("AI","False")), True)
 OPENAI_API = environ.get("OPENAI_API", " ")
 GOOGLE_API_KEY = environ.get("GOOGLE_API_KEY", "") #DON'T REMOVE ANYTHING!! 
-AI_LOGS = int(environ.get("AI_LOGS", "##########")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
+AI_LOGS = int(environ.get("AI_LOGS", "-1002116439783")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
 
 #Auto approve 
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '0').split()]
@@ -100,8 +100,8 @@ BOT_NAME = environ.get("BOT_NAME", "TG Movie Club")
 BOT_ID = environ.get("BOT_ID", "6950960103")
 S_GROUP = environ.get('S_GROUP', "")
 S_CHANNEL = environ.get('S_CHANNEL', "")
-GRP_LNK = environ.get('GRP_LNK', '##########')
-CHNL_LNK = environ.get('CHNL_LNK', '@TG_LinkBotz')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/HINDI_MOVIE_BOTZ')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TG_LinkBotz')
 TUTORIAL = environ.get('TUTORIAL', '') # Tutorial video link for opening shortlink website 
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : TG Botz')
@@ -161,7 +161,7 @@ else:
     URL = "https://afshs-43f3f947f5a1.herokuapp.com/".   format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '##########✅✅##########'))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002066790849'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
